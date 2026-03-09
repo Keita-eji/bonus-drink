01c96ab36f0e4de0f84f432644cd7464af3d9ccd
Keita-eji <ejimasyu@gmail.com>
2026-03-09 16:21:27 +0900

    Update bonus_drink.rb

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,17 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    drinks = amount
+    empty = drinks
+    total = drinks
+
+    while empty >= 3
+      new_drink = empty / 3
+      total += new_drink
+      empty = new_drink + (empty % 3)
+    end
+
+    total
   end
 end
+
+puts "合計: #{BonusDrink.total_count_for(100)}本"
